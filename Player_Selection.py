f6ae4a2f1e41733e4cf14934886146b248f2378c
Enternium <40383888+Enternium@users.noreply.github.com>
2024-12-29 09:50:43 +0000

    6 Player Fixed
    And Simon added

--- Player_Selection.py
+++ Player_Selection.py
@@ -115,7 +115,8 @@ def Run_Player_Selection():
                       Player_Button('Annie', 'Orange', 2, True),
                       Player_Button('Mark', 'White', 3, True),
                       Player_Button('Smele', 'Green', 4, False),
-                      Player_Button('Hilary', 'Red', 5, False)]
+                      Player_Button('Hilary', 'Red', 5, False),
+                      Player_Button('Simon', 'Blue', 6, False)]
     
     go_button = Go_Button()
     
